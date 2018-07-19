d6338183fb6733b8174ca393bce228b67ff23cd2
İhsan Baki Doğan <37176057+ihsanbakidogan@users.noreply.github.com>
2018-07-19 11:56:47 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,18 +1,3 @@
-//consts (for glitch)
-// GEREKLİ YERLER
-const express = require('express');
-const app = express();
-const http = require('http');
-    app.get("/", (request, response) => {
-    console.log(` az önce pinglenmedi. Sonra ponglanmadı... ya da başka bir şeyler olmadı.`);
-    response.sendStatus(200);
-    });
-    app.listen(process.env.PORT);
-    setInterval(() => {
-    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-    }, 280000);
-// GEREKLİ YERLER
-// -------------------------------------------------------------
 const Discord = require('discord.js');
 const ytdl = require('ytdl-core');
 const client = new Discord.Client();
